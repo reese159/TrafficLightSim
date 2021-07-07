cde53acdbb37fbf1ec13cec0d6db42af579b3b5d
reese159 <42494851+reese159@users.noreply.github.com>
2021-07-07 11:41:06 -0400

    Add project files.

---
+++ TrafficLightSim/View/LightDisplayControl.xaml.cs
@@ -0,0 +1,79 @@
+﻿using System.Timers;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using TrafficLightSim.ViewModel;
+
+namespace TrafficLightSim.View
+{
+    /// <summary>
+    /// Interaction logic for LightDisplayControl.xaml
+    /// </summary>
+    public partial class LightDisplayControl : UserControl
+    {
+        private Timer timer;
+
+        public Timer Timer
+        {
+            get { return timer; }
+            set { timer = value; }
+        }
+
+        private static LightTimer lightTimer = new LightTimer();
+
+        public LightDisplayControl()
+        {
+            InitializeComponent();
+            //timer = new System.Timers.Timer();
+            //timer.Elapsed += new ElapsedEventHandler(LightSwitch);
+            //timer.Interval = 2000;
+            //timer.AutoReset = true;
+            //timer.Enabled = true;
+        }
+
+        /// <summary>
+        /// Property definition for Green Light Opacity
+        /// </summary>
+        public double GreenLightOpacity
+        {
+            get { return (double)GetValue(GreenLightOpacityProperty); }
+            set { SetValue(GreenLightOpacityProperty, value); }
+        }
+
+        /// <summary>
+        /// DependencyProperty set for Green Light Opacity
+        /// </summary>
+        public static readonly DependencyProperty GreenLightOpacityProperty =
+            DependencyProperty.Register("GreenLightOpacity", typeof(double), typeof(LightDisplayControl), new FrameworkPropertyMetadata(lightTimer.GreenOpac));
+
+        /// <summary>
+        /// Property definition for Yellow Light Opacity
+        /// </summary>
+        public double YellowLightOpacity
+        {
+            get { return (double)GetValue(YellowLightOpacityProperty); }
+            set { SetValue(YellowLightOpacityProperty, value); }
+        }
+
+        /// <summary>
+        /// DependencyProperty set for Yellow Light Opacity
+        /// </summary>
+        public static readonly DependencyProperty YellowLightOpacityProperty =
+        DependencyProperty.Register("YellowLightOpacity", typeof(double), typeof(LightDisplayControl), new FrameworkPropertyMetadata(lightTimer.YellowOpac));
+
+        /// <summary>
+        /// Property definition for Red Light Opacity
+        /// </summary>
+        public double RedLightOpacity
+        {
+            get { return (double)GetValue(RedLightOpacityProperty); }
+            set { SetValue(RedLightOpacityProperty, value); }
+        }
+
+        /// <summary>
+        /// DependencyProperty set for Red Light Opacity
+        /// </summary>
+        public static readonly DependencyProperty RedLightOpacityProperty =
+        DependencyProperty.Register("RedLightOpacity", typeof(double), typeof(LightDisplayControl), new FrameworkPropertyMetadata(lightTimer.RedOpac));
+    }
+}
---
+++ TrafficLightSim/ViewModel/LightTimer.cs
@@ -0,0 +1,65 @@
+﻿using System.ComponentModel;
+
+namespace TrafficLightSim.ViewModel
+{
+    public class LightTimer : INotifyPropertyChanged
+    {
+        private double greenOpac;
+
+        public double GreenOpac
+        {
+            get { return greenOpac; }
+            set
+            {
+                greenOpac = value;
+                if (PropertyChanged != null)
+                    PropertyChanged(this, new PropertyChangedEventArgs("greenOpac"));
+            }
+        }
+
+        private double yellowOpac;
+
+        public double YellowOpac
+        {
+            get { return yellowOpac; }
+            set
+            {
+                yellowOpac = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("yellowOpac"));
+            }
+        }
+
+        private double redOpac;
+
+        public double RedOpac
+        {
+            get { return redOpac; }
+            set
+            {
+                redOpac = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("redOpac"));
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        /// <summary>
+        /// Create timer based on color of selected light
+        /// </summary>
+        /// <param name="lightColor">Color of selected light</param>
+        public LightTimer()
+        {
+            GreenOpac = 1.0;
+            YellowOpac = 1.0;
+            RedOpac = 1.0;
+            LightSwitch();
+        }
+
+        public void LightSwitch()
+        {
+            GreenOpac = 0.3;
+            YellowOpac = 0.3;
+            RedOpac = 0.3;
+        }
+    }
+}
